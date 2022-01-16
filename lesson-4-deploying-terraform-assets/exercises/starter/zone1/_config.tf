649cecee1c5edb713f222a2d2fe4de0bfd4c9ac1
Trevor Roberts Jr <Trevor.Roberts.Jr@gmail.com>
2022-01-16 16:55:28 -0500

    complete-Deploying Assets via Terraform

--- lesson-4-deploying-terraform-assets/exercises/starter/zone1/_config.tf
+++ lesson-4-deploying-terraform-assets/exercises/starter/zone1/_config.tf
@@ -1,6 +1,6 @@
 terraform {
    backend "s3" {
-     bucket = "udacity-tf-tscotto6"
+     bucket = "udacity-tf-trevor01"
      key    = "terraform/terraform.tfstate"
      region = "us-east-2"
    }
