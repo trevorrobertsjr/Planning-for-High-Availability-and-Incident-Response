844eaff70b527eb394f689cae5ad2972a9443fc3
Trevor Roberts Jr <Trevor.Roberts.Jr@gmail.com>
2022-01-23 15:40:38 -0500

    complete cross-region replica cluster

--- lesson-5-rds-deploy/exercises/solution/_config.tf
+++ lesson-5-rds-deploy/exercises/solution/_config.tf
@@ -1,21 +1,21 @@
 terraform {
-   backend "s3" {
-     bucket = "udacity-tf-travis5"
-     key    = "terraform/terraform.tfstate"
-     region = "us-east-2"
-   }
- }
+  backend "s3" {
+    bucket = "udacity-tf-ud-trevor"
+    key    = "terraform/terraform.tfstate"
+    region = "us-east-2"
+  }
+}
 
- provider "aws" {
-   region = "us-east-2"
-   #profile = "default"
-   
-   default_tags {
-     tags = local.tags
-   }
- }
+provider "aws" {
+  region = "us-east-2"
+  #profile = "default"
 
- provider "aws" {
+  default_tags {
+    tags = local.tags
+  }
+}
+
+provider "aws" {
   alias  = "usw1"
-  region = "us-west-1"
+  region = "us-west-2"
 }
--- lesson-5-rds-deploy/exercises/starter/_config.tf
+++ lesson-5-rds-deploy/exercises/starter/_config.tf
@@ -1,20 +1,28 @@
 terraform {
-   backend "s3" {
-     bucket = "udacity-tf-travis5"
-     key    = "terraform/terraform.tfstate"
-     region = "us-east-2"
-   }
- }
+  backend "s3" {
+    bucket = "udacity-tf-ud-trevor"
+    key    = "terraform/terraform.tfstate"
+    region = "us-east-2"
+  }
+  # required_providers {
+  #   aws = {
+  #     source                = "hashicorp/aws"
+  #     version               = ">=3.73.0"
+  #     configuration_aliases = [aws.usw1]
+  #   }
+  # }
 
- provider "aws" {
-   region = "us-east-2"
-   
-   default_tags {
-     tags = local.tags
-   }
- }
+}
 
- provider "aws" {
+provider "aws" {
+  region = "us-east-2"
+
+  default_tags {
+    tags = local.tags
+  }
+}
+
+provider "aws" {
   alias  = "usw1"
-  region = "us-west-1"
+  region = "us-west-2"
 }
